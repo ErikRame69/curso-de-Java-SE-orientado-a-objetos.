44b92325ed2e026d8ba5218a7cbba04b7bbd7961
Erik Daniel <erikrame69@gmail.com>
2021-07-17 17:47:28 -0500

    Final: Variables Constantes

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,7 @@
-import ui.UiMenu;
-
 import static ui.UiMenu.*;
 
 public class Main {
     public static void main(String[] args) {
-
-        Doctor myDoctor = new Doctor();
-        myDoctor.name = "Ximena Valerio";
-        myDoctor.showName();
-        myDoctor.showId();
-
-        Doctor myDoctorAnn = new Doctor();
-        myDoctor.showId();
-
-        UiMenu.showMenu();
+        showMenu();
     }
 }
