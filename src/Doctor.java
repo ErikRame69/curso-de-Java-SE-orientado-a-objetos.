a6bc66a7d07e4db0cc6486a6a05e7592b98db78c
Erik Daniel <erikrame69@gmail.com>
2021-07-17 17:24:36 -0500

    Creando elementos estático

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,10 +1,22 @@
 public class Doctor {
-    int id;
+    static int id = 0;
     String name;
     String speciality;
+
+    Doctor(){
+        System.out.println("Construyenco el metodo constructor");
+        id++;
+    }
+
+    Doctor(String name){
+        System.out.println("El nombre del doctor asignado es: "+ name);
+    }
 
     //Comportamientos
     public void showName(){
         System.out.println(name);
+    }
+    public void showId(){
+        System.out.println("ID Doctor: "+id);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
+import ui.UiMenu;
+
+import static ui.UiMenu.*;
+
 public class Main {
     public static void main(String[] args) {
 
         Doctor myDoctor = new Doctor();
         myDoctor.name = "Ximena Valerio";
         myDoctor.showName();
+        myDoctor.showId();
 
+        Doctor myDoctorAnn = new Doctor();
+        myDoctor.showId();
+
+        UiMenu.showMenu();
     }
 }
