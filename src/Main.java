0ef4671afb51f47f99dcb33428390f3bd5fc3e53
Erik Daniel <erikrame69@gmail.com>
2021-07-18 03:01:43 -0500

    Clases anonimas

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 import model.Doctor;
+import model.ISchedulable;
 import model.Patient;
 import model.User;
 
 import java.util.Date;
+import java.util.SortedMap;
 
 public class Main {
     public static void main(String[] args) {
@@ -21,6 +23,18 @@ public class Main {
 
     User userPa = new Patient("Cuasimodo", "Cuasimodo@fotos.com");
     userPa.showDataUser();
+
+    User user1 = new User("Daniel", "Daniel@user.com") {
+        @Override
+        public void showDataUser() {
+            System.out.println("Doctor");
+            System.out.println("Hospital: Polanco");
+            System.out.println("Departamento: Neurologia");
+        }
+    };
+    user1.showDataUser();
+
+
     /*
     for (model.Doctor.AvailableAppointment aA : myDoctor.getAvailableAppointments()) {
         System.out.println(aA.getDate() + "  " + aA.getTime());
